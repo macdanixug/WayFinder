f29fac68b330032371c6c8f866235e264f7ced35
macdanixug <relnusdan1@gmail.com>
2023-06-02 16:41:48 +0300

    Mbarara Regional Referral Map

--- app/src/main/java/com/example/wayfinder/Map_Fragment.java
+++ app/src/main/java/com/example/wayfinder/Map_Fragment.java
@@ -14,9 +14,12 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Map_Fragment extends Fragment {
     public Map_Fragment() {
@@ -32,18 +35,38 @@ public class Map_Fragment extends Fragment {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
                 LatLng mbararaReferralHospital = new LatLng(-0.616389, 30.658889); // Latitude and Longitude for Mbarara Regional Referral Hospital
-                MarkerOptions markerOptions = new MarkerOptions()
-                        .position(mbararaReferralHospital)
-                        .title("Mbarara Regional Referral Hospital")
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)); // Customize marker color
-                googleMap.addMarker(markerOptions);
+
+                // Create a list of LatLng points to define the perimeter of the hospital
+                List<LatLng> perimeterPoints = new ArrayList<>();//0.617715, 30.657336
+                perimeterPoints.add(new LatLng(-0.616449, 30.657642));
+                perimeterPoints.add(new LatLng(-0.616518, 30.657529));
+                perimeterPoints.add(new LatLng(-0.617715, 30.657336));
+                perimeterPoints.add(new LatLng(-0.617677, 30.657781));
+                perimeterPoints.add(new LatLng(-0.617661, 30.657996));
+                perimeterPoints.add(new LatLng(-0.617473, 30.658114));
+                perimeterPoints.add(new LatLng(-0.617409, 30.658501));
+                perimeterPoints.add(new LatLng(-0.616996, 30.658469));
+                perimeterPoints.add(new LatLng(-0.616856, 30.658383));
+                perimeterPoints.add(new LatLng(-0.617473, 30.658114));
+//                perimeterPoints.add(new LatLng(-0.617473, 30.658114));
+
+
+                // Create a polygon options object and add the perimeter points
+                PolygonOptions polygonOptions = new PolygonOptions()
+                        .addAll(perimeterPoints)
+                        .strokeWidth(5) // Set the stroke width of the polygon outline
+                        .strokeColor(0xFF0000FF) // Set the stroke color of the polygon outline (blue)
+                        .fillColor(0x220000FF); // Set the fill color of the polygon (semi-transparent blue)
+
+                // Add the polygon overlay to the map
+                Polygon polygon = googleMap.addPolygon(polygonOptions);
+
+                // Adjust the camera position to show the polygon
                 googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mbararaReferralHospital, 17)); // Adjust the zoom level to display the full view
 
-                // Disable gestures to restrict user interaction with the map
-                googleMap.getUiSettings().setAllGesturesEnabled(false);
-                googleMap.getUiSettings().setZoomControlsEnabled(false);
-
+                // Enable gestures and zoom controls
                 UiSettings uiSettings = googleMap.getUiSettings();
+                uiSettings.setAllGesturesEnabled(true);
                 uiSettings.setZoomControlsEnabled(true);
             }
         });
